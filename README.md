f603ca589168db4b3bf2b2e38c9660619e5344e5
gajirou <lifelongsre@gmail.com>
2023-07-12 11:22:45 +0900

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # feedenostr
-feed を deno で nostr に投稿する。
+feed を deno で nostr に投稿する。  
+実行時間から指定した取得基準となる分の前に投稿された記事を取得し POST する。
+
+# 環境変数
+| 環境変数名 | 設定値 |
+| ---- | ---- |
+| FEED_URL | 取得する feed の URL |
+| PRIVATE_KEY | POST 時に利用するプライベートキー |
+| TARGET_MINUTES | feed 取得基準となる分 |
+
+# 利用方法
+```
+# ビルド
+docker build -t feedenostr -f ./docker/Dockerfile .
+
+# ローカルでは src をマウントして作業
+docker run -it --rm --mount type=bind,source="$(pwd)"/src,target=/srv -w /srv \
+  -e FEED_URL="https://xxxxxxxxx.xx/" \
+  -e PRIVATE_KEY="nsecxxxxxxxxxxxxxxxxxxxxx" \
+  -e TARGET_MINUTES=30 \
+  feedenostr bash
+```
---
+++ src/feedenostr.ts
@@ -0,0 +1,48 @@
+import { parseFeed, Nostr, Relay } from './deps.ts';
+
+const relayList = {
+  "Eden"    : "wss://eden.nostr.land",
+  "Damus"   : "wss://relay.damus.io",
+  "Nokotaro": "wss://nostr-relay.nokotaro.com",
+  //"Relay-jp": "wss://relay-jp.nostr.wirednet.jp",
+  "Relay.jp": "wss://relay.nostr.wirednet.jp"
+};
+
+const url           = Deno.env.get("FEED_URL");
+const privateKey    = Deno.env.get("PRIVATE_KEY");
+const targetMinutes = Deno.env.get("TARGET_MINUTES");
+const pastDate      = Date.now() - (60 * targetMinutes * 1000);
+
+const response = await fetch(url);
+const xml      = await response.text();
+const feed     = await pareFeed(xml);
+
+const nostr = new Nostr();
+nostr.privateKey = privateKey;
+
+for(const key in relayList) {
+  const value = relayList[key];
+  nostr.relayList.push({
+    name: key,
+    url : value
+  });
+}
+
+nostr.on('relayConnected', (relay: Relay) => console.log('Relay connected.', relay.name));
+nostr.on('relayError', (err: Error) => console.log('Relay error;', err));
+nostr.on('relayNotice', (notice: string[]) => console.log('Notice', notice));
+
+nostr.debugMode = true;
+await nostr.connect();
+
+for (const entrie of feed.entries) {
+  const publishedDate = new Date(entrie.published).getTime();
+  if(publishedDate <= pastDate) {
+    continue;
+  }
+  await nostr.sendTextPost(entrie.title.value + "\n" + entrie.links[0].href);
+}
+
+await nostr.disconnect();
+
+Deno.exit(0);
