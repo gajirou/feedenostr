78f4c53da86e497e6c2ffb7bcbf6e955f1f55e1d
gaji <110142614+gajirou@users.noreply.github.com>
2023-08-28 11:52:44 +0900

    from の設定を " に統一

--- src/deps.ts
+++ src/deps.ts
@@ -1,2 +1,2 @@
 export { parseFeed } from "https://deno.land/x/rss/mod.ts";
-export { Nostr, Relay } from 'https://deno.land/x/nostr_deno_client/mod.ts';
+export { Nostr, Relay } from "https://deno.land/x/nostr_deno_client/mod.ts";
